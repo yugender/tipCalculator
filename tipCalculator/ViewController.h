bda6300fa6ba94b4f9836f3fe5a8bc607fe0eaf7
Yugender Boini <yboini@yahoo-inc.com>
2017-01-14 19:52:51 -0800

    Initial Commit

---
+++ tipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipCalculator
+//
+//  Created by  Yugender Boini on 1/14/17.
+//  Copyright © 2017 sample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
